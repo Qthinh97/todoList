5b19372881491bd358b828645c8445da733753cd
Qthinh97 <quocthinh8712@gmail.com>
2023-03-26 22:23:31 +0700

    todoList-react

--- src/pages/user/ProductDetail/index.jsx
+++ src/pages/user/ProductDetail/index.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 
 function ProductDetailPage() {
   const params = useParams();
-  console.log("🚀 ~ ProductDetailPage ~ params:", params);
   return <div>Product Detail Page - {params.id}</div>;
 }
 
