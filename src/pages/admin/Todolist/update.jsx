00d9929cc049616d2a660c398bb7378133600070
Qthinh97 <quocthinh8712@gmail.com>
2023-04-12 19:12:41 +0700

    test 1223

--- src/pages/admin/Todolist/update.jsx
+++ src/pages/admin/Todolist/update.jsx
@@ -99,6 +99,9 @@ function Update({ id, title, content }) {
         <Button danger onClick={() => dispatch(deleteToDoListAction({ id }))}>
           Delete
         </Button>
+        <Button danger onClick={() => dispatch(deleteToDoListAction({ id }))}>
+          Delete
+        </Button>
       </Space>
     </Card>
   );
